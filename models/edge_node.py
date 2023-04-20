8df84b1268e811ae75c3fbd7aea7a312dfc67c41
ivan <idimitryz@gmail.com>
2023-04-20 12:38:46 +0200

    refactor: remove unused methods from edge_node
    remove store and get resource from edge nodes, since those operations should be done in the cache object

--- models/edge_node.py
+++ models/edge_node.py
@@ -1,8 +1,6 @@
 from parameters import CACHE_DEFAULT_SIZE
 from models.cache import Cache
 
-import time
-
 
 class EdgeNode:
     """
@@ -40,30 +38,5 @@ class EdgeNode:
         self.x = x
         self.y = y
 
-    def store_resource(self, resource_id: str, validity_time):
-        if self.cache.current_size < self.cache.max_size:
-            self.cache.add_resource(resource_id, validity_time)
-            print(
-                f"Resource {resource_id} stored in cache of EdgeNode {self.edge_node_id}")
-        else:
-            print(
-                f"Cache of EdgeNode {self.edge_node_id} is full. Resource {resource_id} not stored.")
-
-    def retrieve_resource(self, resource_id: str):
-        for resource in self.cache.resources:
-            if resource.resource_id == resource_id:
-                current_time = time.time()
-                if current_time - resource.storage_time <= resource.validity_time:
-                    print(
-                        f"Resource {resource_id} retrieved from cache of EdgeNode {self.edge_node_id}")
-                    return resource
-                else:
-                    print(
-                        f"Resource {resource_id} has expired in cache of EdgeNode {self.edge_node_id}.")
-                    self.cache.remove_expired_resources()
-                    return None
-        print(
-            f"Resource {resource_id} not found in cache of EdgeNode {self.edge_node_id}.")
-        return None
     def __str__(self):
         return f"Edge Node {self.id} - Current Position: {self.get_position()}"
