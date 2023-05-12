53ad8341a16f305089a8d201cde84ffd050f5b71
ivan <idimitryz@gmail.com>
2023-05-12 11:50:11 +0200

    experiments: refactor experiments name to follow a pattern

--- script_simulator.py
+++ script_simulator.py
@@ -2,12 +2,13 @@ import subprocess
 
 replications = 30
 labels_of_already_done_experiments = ["accuracy-0-N0-standard-cache-mode-mix",
+                                      "accuracy-0-N1-standard-cache-mode-mix",
                                       "accuracy-0-N2-standard-cache-mode-mix",
                                       "accuracy-20-N0-standard-mix",
                                       "accuracy-20-N1-standard-mix",
                                       "accuracy-20-N2-standard-mix",
                                       "accuracy-40-N0-standard-mix",
-                                      "accuracy-40-N1-standard-mix",]
+                                      "accuracy-40-N1-standard-mix", ]
 # Define list of parameters to vary
 accuracy_list = [0, 0.2, 0.4]  # 3
 cache_not_found_list = [True, False]  # 2
@@ -35,7 +36,7 @@ for accuracy in accuracy_list:
                         label += "type"
                     else:
                         label += "mix"
-                    if(not label in labels_of_already_done_experiments):
+                    if (not label in labels_of_already_done_experiments):
                         # Define command to run simulation with current parameter settings
                         command = f"python3 simulator.py --label \"{label}\" --accuracy {accuracy} --cache-not-found-resource {cache_not_found} --neighbor-edge-nodes {neighbor_edge_nodes} --cache-mode \"{cache_mode}\" --replications {replications} --user-distribution-id {user_distribution['id']} --user-distribution-type {user_distribution['type']} --user-distribution-location {user_distribution['location']}"
                         # Print current parameter settings
